11ff19e30dcfd00f5f1b6e46cb17697bd8cc3c57
Rasesh Ramanuj <rasesh94@gmail.com>
2018-01-08 00:25:26 +0000

    trying to switch to manual controls upon border detection, errors everywhere

--- PT_Assignment/PT_Assignment.ino
+++ PT_Assignment/PT_Assignment.ino
@@ -8,7 +8,7 @@
 #define LED_PIN 13
 
 // this might need to be tuned for different lighting conditions, surfaces, etc.
-#define QTR_THRESHOLD  500 // microseconds
+#define QTR_THRESHOLD  1000 // microseconds
 
 // Speed/duration settings
 
@@ -18,39 +18,43 @@
 #define REVERSE_DURATION  250
 #define TURN_DURATION     200
 
+Pushbutton button(ZUMO_BUTTON);
 ZumoBuzzer buzzer;
 ZumoReflectanceSensorArray reflectanceSensors(QTR_NO_EMITTER_PIN);
 ZumoMotors motors;
 
 #define NUM_SENSORS 6
 unsigned int sensor_values[NUM_SENSORS];
-
+bool line_detection();
 void setup()
 {
+    Serial.println("Press button for Callibration!");
+    button.waitForButton();
     sensor_callibration();
     // Init LED pin to enable it to be turned on later
     pinMode(LED_PIN, OUTPUT);
-
+    
     // Set up serial monitor for inital testing purposes
     Serial.begin(9600);
-    Serial.println("waiting...");
+  //  Serial.println("waiting...");
+  
+    Serial.println("Put zumo at the start, and press button to begin!!");
+    button.waitForButton();
+    start_robot();
 }
 
 void loop()
 {
-        char inputChar = (char) Serial.read();
-        switch (inputChar){
-           case 'w': case 'W': digitalWrite(LED_PIN, HIGH); motors.setLeftSpeed(FORWARD_SPEED); motors.setRightSpeed(FORWARD_SPEED); delay(2); break;
-           case 'a': case 'A': digitalWrite(LED_PIN, HIGH); motors.setLeftSpeed(0); motors.setRightSpeed(FORWARD_SPEED); delay(2); break;
-           case 's': case 'S': digitalWrite(LED_PIN, HIGH); motors.setLeftSpeed(-REVERSE_SPEED); motors.setRightSpeed(-REVERSE_SPEED); delay(2); break;
-           case 'd': case 'D': digitalWrite(LED_PIN, HIGH); motors.setLeftSpeed(FORWARD_SPEED); motors.setRightSpeed(0); delay(2); break;
-           case 'q': case 'Q': digitalWrite(LED_PIN, HIGH); motors.setLeftSpeed(0); motors.setRightSpeed(0); delay(2); break;
-        }
-  if (Serial.available()>0)
-  {
-   /* char inputChar = (char) Serial.read();
-    if (inputChar == "W" || "w")
-    {*/
+    reflectanceSensors.read(sensor_values);
+  if (line_detection){
+    manual_control();
+    }
+    else{
+      start_robot();
+      }
+ 
+}
+ bool line_detection(){
       reflectanceSensors.read(sensor_values);
       if (sensor_values[0] > QTR_THRESHOLD)
       {
@@ -60,27 +64,48 @@ void loop()
         motors.setSpeeds(TURN_SPEED, -TURN_SPEED);
         delay(TURN_DURATION);
         motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
+        return true;
       }
       else if (sensor_values[5] > QTR_THRESHOLD)
       {
         // if rightmost sensor detects line, reverse and turn to the left
-        motors.setSpeeds(-REVERSE_SPEED, -REVERSE_SPEED);
+      motors.setSpeeds(-REVERSE_SPEED, -REVERSE_SPEED);
         delay(REVERSE_DURATION);
         motors.setSpeeds(-TURN_SPEED, TURN_SPEED);
         delay(TURN_DURATION);
         motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
+           return true;
       }
-      else
-      {
-        // otherwise, go straight
-        motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
-      }      
+          return false;
+      }         
+  void manual_control()
+  {
+      motors.setLeftSpeed(0); motors.setRightSpeed(0);
+
+    char inputChar;
+      Serial.println("Press Complete when turning has finished!");
+      while (!inputChar == "C"){
+                    inputChar = (char) Serial.read();
+
+        switch (inputChar)
+        {
+           case 'w': case 'W': digitalWrite(LED_PIN, HIGH); motors.setLeftSpeed(FORWARD_SPEED); motors.setRightSpeed(FORWARD_SPEED); delay(2); break;
+           case 'a': case 'A': digitalWrite(LED_PIN, HIGH); motors.setLeftSpeed(0); motors.setRightSpeed(FORWARD_SPEED); delay(2); break;
+           case 's': case 'S': digitalWrite(LED_PIN, HIGH); motors.setLeftSpeed(-REVERSE_SPEED); motors.setRightSpeed(-REVERSE_SPEED); delay(2); break;
+           case 'd': case 'D': digitalWrite(LED_PIN, HIGH); motors.setLeftSpeed(FORWARD_SPEED); motors.setRightSpeed(0); delay(2); break;
+           case 'q': case 'Q': digitalWrite(LED_PIN, HIGH); motors.setLeftSpeed(0); motors.setRightSpeed(0); delay(2); break;
+        }
     }
   }
-
-  void manual_control(){}
+        
   void start_robot(){
     
+        motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
+         if (line_detection)
+ {
+  Serial.println("Corner found, please navigate manually");
+  manual_control();
+ }  
   }
   void sensor_callibration(){
    // Initialise the reflectance sensors module
@@ -91,14 +116,14 @@ void loop()
   digitalWrite(13, HIGH);        // turn on LED to indicate we are in calibration mode
 
  unsigned long startTime = millis();
-  while(millis() - startTime < 10000)   // make the calibration take 10 seconds
+  while(millis() - startTime < 5000)   // make the calibration take 5 seconds
   {
         //turn left
         motors.setSpeeds(-TURN_SPEED, TURN_SPEED);
-        delay(TURN_DURATION);
+        delay(2500);
         //turn right
         motors.setSpeeds(TURN_SPEED, -TURN_SPEED);
-        delay(TURN_DURATION);
+        delay(2500);
         reflectanceSensors.calibrate();
   }
   motors.setLeftSpeed(0); motors.setRightSpeed(0); delay(2);
